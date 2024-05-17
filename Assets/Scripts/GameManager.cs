208b523df61164519bbcb08cc77426c041fdf3e5
Marcos Moran <marcosmoran62@hotmail.com>
2024-05-17 19:26:05 +0100

    Bug fix on transition

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -37,7 +37,7 @@ public class GameManager : MonoBehaviour
     }
     IEnumerator TransitionCoroutine()
     {
-        while(_transition.transitionFinished)
+        while(!_transition.transitionFinished)
         {
             yield return null;
         }
